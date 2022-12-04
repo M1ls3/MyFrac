d865d14e4245d7c5eec42fd420187a3c3ea8f455
M1lc3 <sofarua@gmail.com>
2022-12-04 16:46:40 +0200

    Commit 1

---
+++ Lab_2(2)/Lab2(2)/MyFrac.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2_2_
+{
+    class MyFrac
+    {
+        protected long nom, denom;
+
+        public MyFrac(long nom_, long denom_)
+        {
+            if (denom_ == 0) { throw new Exception("Denominator cannot be 0 or lower!"); }
+            if (denom_ < 0) { nom = -nom_; denom = -denom_; }
+            else { nom = nom_; denom = denom_; }
+            nom_ = Math.Abs(nom_);
+            denom_ = Math.Abs(denom_);
+            while (nom_ != 0 && denom_ != 0)
+            {
+                if (nom_ > denom_) { nom_ = nom_ % denom_; }
+                else { denom_ = denom_ % nom_; }
+            }
+            nom /= nom_ + denom_;
+            denom /= nom_ + denom_;
+        }
+
+        public override string ToString() { return nom + "/" + denom; }
+        public static string ToStringWithIntegerPart(MyFrac f)
+        {
+            if (Math.Abs(f.nom) > f.denom)
+            {
+                string negative = f.nom < 0 ? "-" : "";
+                return negative + "(" + Math.Abs(f.nom) / f.denom + "+" + Math.Abs(f.nom) % f.denom + "/" + f.denom + ")";
+            }
+            else { return f.ToString(); }
+        }
+
+        public static double DoubleValue(MyFrac f) { return (double)f.nom / (double)f.denom; }
+        public static MyFrac operator +(MyFrac f1, MyFrac f2) { return new MyFrac(f1.nom * f2.denom + f2.nom * f1.denom, f1.denom * f2.denom); }
+        public static MyFrac operator -(MyFrac f1, MyFrac f2) { return new MyFrac(f1.nom * f2.denom - f2.nom * f1.denom, f1.denom * f2.denom); }
+        public static MyFrac operator *(MyFrac f1, MyFrac f2) { return new MyFrac(f1.nom * f2.nom, f1.denom * f2.denom); }
+        public static MyFrac operator /(MyFrac f1, MyFrac f2) { return new MyFrac(f1.nom * f2.denom, f1.denom * f2.nom); }
+
+        public static MyFrac CalcSum1(int n)
+        {
+            MyFrac res = new MyFrac(0, 1);
+            for (long i = 1; i <= n; i++)
+            {
+                MyFrac add = new MyFrac(1, i * (i + 1));
+                res = (res + add);
+            }
+            return res;
+        }
+
+        public static MyFrac CalcSum2(int n)
+        {
+            MyFrac res = new MyFrac(1, 1), unit = new MyFrac(1, 1);
+            for (long i = 2; i <= n; i++)
+            {
+                MyFrac fracForMinus = new MyFrac(1, i * i);
+                MyFrac mult = (unit - fracForMinus);
+                res = (res * mult);
+            }
+            return res;
+        }
+    }
+}
---
+++ Lab_2(2)/Lab2(2)/Program.cs
@@ -0,0 +1,52 @@
+﻿using Lab2_2_;
+using Microsoft.VisualBasic.FileIO;
+using System;
+using System.Linq;
+using System.Threading.Channels;
+
+namespace Task2_MyFrac
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\nInput first Frac: ");
+            string[] value = Console.ReadLine().Trim().Split();
+            MyFrac f_frac = new MyFrac(Convert.ToInt32(value[0]), Convert.ToInt32(value[1]));
+            value = null ;
+
+            Console.WriteLine("\nInput secound Frac: ");
+            value = Console.ReadLine().Trim().Split();
+            MyFrac s_frac = new MyFrac(Convert.ToInt32(value[0]), Convert.ToInt32(value[1]));
+
+            Console.WriteLine($"\nFirst Frac: {f_frac}; Secound Frac: {s_frac}");
+
+            Console.WriteLine($"\nThe first fraction: {MyFrac.ToStringWithIntegerPart(f_frac)}");
+            Console.WriteLine($"The second fraction: {MyFrac.ToStringWithIntegerPart(s_frac)}");
+            Console.WriteLine();
+
+            Console.WriteLine($"\nThe first fraction in decimal form: {MyFrac.DoubleValue(f_frac)}");
+            Console.WriteLine($"The second fraction in decimal form: {MyFrac.DoubleValue(s_frac)}");
+            Console.WriteLine();
+
+            Console.WriteLine("\nAdding:");
+            Console.WriteLine(MyFrac.ToStringWithIntegerPart(f_frac + s_frac));
+            Console.WriteLine();
+
+            Console.WriteLine("\nSubtraction");
+            Console.WriteLine(MyFrac.ToStringWithIntegerPart(f_frac - s_frac));
+            Console.WriteLine();
+
+            Console.WriteLine("\nMultiplication");
+            Console.WriteLine(MyFrac.ToStringWithIntegerPart(f_frac * s_frac));
+            Console.WriteLine();
+
+            Console.WriteLine("\nDivision");
+            Console.WriteLine(MyFrac.ToStringWithIntegerPart(f_frac / s_frac));
+            Console.WriteLine();
+
+            Console.WriteLine(MyFrac.ToStringWithIntegerPart(MyFrac.CalcSum1(4)));
+            Console.WriteLine(MyFrac.ToStringWithIntegerPart(MyFrac.CalcSum2(4)));
+        }
+    }
+}
